aa77c7dd2b451b393a942b6270f269b31eab702c
Sarthak1918 <pandasarthak12345@gmail.com>
2023-07-05 00:27:40 +0530

    code update
    pre deploy minor correction

--- src/Components/Images.jsx
+++ src/Components/Images.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import Image from './Image'
 import '../styles/Images.css'
 
--- src/Components/LightBox.jsx
+++ src/Components/LightBox.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import "../styles/LightBox.css"
 import download from '../download'
 
